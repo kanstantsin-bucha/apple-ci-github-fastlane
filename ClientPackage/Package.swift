13a06b2b2186e8674b7a090d394098314b759d23
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2024-02-11 21:14:38 +0100

    Disabled unused packages.

--- ClientPackage/Package.swift
+++ ClientPackage/Package.swift
@@ -24,14 +24,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/johnpatrickmorgan/NavigationBackport", from: "0.9.0"),
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.13.0"),
-        .package(url: "https://github.com/RevenueCat/purchases-ios", from: "4.25.10"),
-        .package(url: "https://github.com/auth0/JWTDecode.swift", from: "3.1.0"),
-        .package(url: "https://github.com/srgtuszy/keychain-swift", branch: "master"),
-        .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0"),
-        .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "4.1.4"),
         .package(url: "https://github.com/OneSignal/OneSignal-iOS-SDK", from: "5.0.2"),
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", from: "6.12.2"),
-        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from: "2.33.4"),
+//        .package(url: "https://github.com/RevenueCat/purchases-ios", from: "4.25.10"),
+//        .package(url: "https://github.com/auth0/JWTDecode.swift", from: "3.1.0"),
+//        .package(url: "https://github.com/srgtuszy/keychain-swift", branch: "master"),
+//        .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0"),
+//        .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "4.1.4"),
+//        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", from: "6.12.2"),
+//        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from: "2.33.4"),
         .package(url: "https://github.com/Quick/Quick", from: "7.2.0"),
         .package(url: "https://github.com/Quick/Nimble", from: "12.2.0")
     ],
@@ -43,16 +43,16 @@ let package = Package(
             dependencies: [
                 "NavigationBackport",
                 .productItem(name: "Sentry", package: "sentry-cocoa", moduleAliases: nil, condition: nil),
-                .productItem(name: "RevenueCat", package: "purchases-ios", moduleAliases: nil, condition: nil),
-                .productItem(name: "JWTDecode", package: "JWTDecode.swift", moduleAliases: nil, condition: nil),
-                .productItem(name: "KeychainSwift", package: "keychain-swift", moduleAliases: nil, condition: nil),
-                .productItem(name: "SwiftUIIntrospect", package: "swiftui-introspect", moduleAliases: nil, condition: nil),
-                .productItem(name: "Mixpanel", package: "mixpanel-swift", moduleAliases: nil, condition: nil),
                 .productItem(name: "OneSignalFramework", package: "OneSignal-iOS-SDK", moduleAliases: nil, condition: nil),
                 .productItem(name: "OneSignalInAppMessages", package: "OneSignal-iOS-SDK", moduleAliases: nil, condition: nil),
-                .productItem(name: "AppsFlyerLib", package: "AppsFlyerFramework", moduleAliases: nil, condition: nil),
-                .productItem(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm", moduleAliases: nil, condition: nil),
-                .productItem(name: "AWSMobileClientXCF", package: "aws-sdk-ios-spm", moduleAliases: nil, condition: nil)
+//                .productItem(name: "RevenueCat", package: "purchases-ios", moduleAliases: nil, condition: nil),
+//                .productItem(name: "JWTDecode", package: "JWTDecode.swift", moduleAliases: nil, condition: nil),
+//                .productItem(name: "KeychainSwift", package: "keychain-swift", moduleAliases: nil, condition: nil),
+//                .productItem(name: "SwiftUIIntrospect", package: "swiftui-introspect", moduleAliases: nil, condition: nil),
+//                .productItem(name: "Mixpanel", package: "mixpanel-swift", moduleAliases: nil, condition: nil),
+//                .productItem(name: "AppsFlyerLib", package: "AppsFlyerFramework", moduleAliases: nil, condition: nil),
+//                .productItem(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm", moduleAliases: nil, condition: nil),
+//                .productItem(name: "AWSMobileClientXCF", package: "aws-sdk-ios-spm", moduleAliases: nil, condition: nil)
             ]
         ),
         .target(
--- ClientPackage/Sources/ClientThirdPartyTarget/ClientThirdPartyTarget.swift
+++ ClientPackage/Sources/ClientThirdPartyTarget/ClientThirdPartyTarget.swift
@@ -1,15 +1,15 @@
 import NavigationBackport
 import Sentry
-import RevenueCat
-import JWTDecode
-import KeychainSwift
-import SwiftUIIntrospect
-import Mixpanel
-import OneSignalFramework
+//import RevenueCat
+//import JWTDecode
+//import KeychainSwift
+//import SwiftUIIntrospect
+//import Mixpanel
+//import OneSignalFramework
 import OneSignalInAppMessages
-import AppsFlyerLib
-import AWSCognitoIdentityProvider
-import AWSMobileClientXCF
+//import AppsFlyerLib
+//import AWSCognitoIdentityProvider
+//import AWSMobileClientXCF
 
 // NavigationBackport
 public typealias AppNavigationPath = NBNavigationPath

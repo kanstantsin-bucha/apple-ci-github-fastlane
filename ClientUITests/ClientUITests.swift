548fd014b8b98995dd408235228d67abf79263a5
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2024-02-13 18:51:12 +0100

    Make UI tests target have an entry point. Fixing a build time

--- ClientUITests/ClientUITests.swift
+++ ClientUITests/ClientUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-//final class ClientUITests: XCTestCase {
+final class ClientUITests: XCTestCase {
 //
 //    override func setUpWithError() throws {
 //        // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -38,4 +38,4 @@ import XCTest
 //            }
 //        }
 //    }
-//}
+}
